f064527c178816e7e479ad78aafa4616230298cd
Exadra37 <exadra37@gmail.com>
2018-04-19 21:59:45 +0100

    --wip-- [skip ci]

--- playground/processes/random_word.exs
+++ playground/processes/random_word.exs
@@ -4,6 +4,7 @@ defmodule Playground.Processes.RandomWord do
 
   # Enum.each(1..100000, fn _ -> spawn(fn -> Process.sleep(5000) end) end)
 
+   #Enum.each(1..10000, fn _ -> spawn(fn number -> number * number end) end)
   def launch(number) do
     words = Dictionary.start()
     randw = fn -> random_word(words) |> greeter() end
@@ -21,5 +22,14 @@ defmodule Playground.Processes.RandomWord do
 
     IO.puts "-> #{name}"
   end
+
+  def init(number) do
+    words = Dictionary.start()
+  end
+
+  def z_combinator f do
+    combinator = fn(x) -> f.(fn(y) -> x.(x).(y) end)
+  end
+end
 
 end
