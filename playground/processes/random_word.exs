b264b90c26daff21f29c1d9aadd0532a5d1df694
Exadra37 <exadra37@gmail.com>
2018-04-19 18:51:49 +0100

    --wip-- [skip ci]

--- playground/processes/random_word.exs
+++ playground/processes/random_word.exs
@@ -2,17 +2,12 @@ defmodule Playground.Processes.RandomWord do
 
   alias Dictionary.WordList
 
-  def start() do
-    Dictionary.start()
-  end
-
   # Enum.each(1..100000, fn _ -> spawn(fn -> Process.sleep(5000) end) end)
 
-  def launch(number, words) do
-    function = fn words -> random_word(words) |> greeter() end
-    #Enum.each(1..2, function)
-    Enum.each(1..10, fn  -> spawn(function) end)
-
+  def launch(number) do
+    words = Dictionary.start()
+    randw = fn -> random_word(words) |> greeter() end
+    Enum.each(1..number, fn _-> spawn(randw) end)
   end
 
   defp random_word(words) do
@@ -24,7 +19,7 @@ defmodule Playground.Processes.RandomWord do
     # just for demo purposes, almost never used in production code
     Process.sleep 1000
 
-    IO.puts "RANDOM WORD: #{name}"
+    IO.puts "-> #{name}"
   end
 
 end
