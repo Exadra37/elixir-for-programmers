18620a36aaec299c861be3b54b3a7a45e7ee0836
Exadra37 <exadra37@gmail.com>
2018-04-26 23:11:30 +0100

    Make the Dictionary a Free Standing Application.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ dictionary/lib/dictionary/application.ex
@@ -0,0 +1,18 @@
+defmodule Dictionary.Application do
+
+  use Application
+
+  @moduledoc """
+  When we start the Dictionary application from mix this function is invoked
+  as per definition in Dictionary.Mixfile.application/, thus we don't need to
+  start in manually.
+
+  Now if we run `iex -S mix` we will just need to invoke the function
+  `Dictionary.WordList.random_word/0` to get our random word back.
+
+  """
+  def start(_type, _args) do
+    Dictionary.WordList.start_link()
+  end
+
+end
