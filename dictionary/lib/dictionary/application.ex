9c84016d2048105261ddbe4056a3a67536244f37
Exadra37 <exadra37@gmail.com>
2018-04-27 22:52:19 +0100

    No, Mr Dictionary, I Expect You To Die.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- dictionary/lib/dictionary/application.ex
+++ dictionary/lib/dictionary/application.ex
@@ -12,7 +12,19 @@ defmodule Dictionary.Application do
 
   """
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+
+    import Supervisor.Spec
+
+    children = [
+      worker(Dictionary.WordList, [])
+    ]
+
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 
 end
